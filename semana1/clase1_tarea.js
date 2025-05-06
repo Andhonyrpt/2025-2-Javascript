f61468eb238da834078a9332349e91a07aedb384
Andhony <andhonyrpt@gmail.com>
2025-05-06 16:18:35 -0600

    Resolución de tarea de clase1

--- semana1/clase1_tarea.js
+++ semana1/clase1_tarea.js
@@ -1,17 +1,45 @@
-// Ejercicio: Crear una calculadora simple que realice operaciones básicas (suma, resta, multiplicación y división) entre dos números.
-
-const { ask } = require('../helpers/input');
+const { ask } = require('../helpers/input.js');
 
 async function main() {
-  const num1 = Number(await ask('Ingresa el primer número: '));
-  const num2 = Number(await ask('Ingresa el segundo número: '));
+  const numero1 = Number(await ask('Inserta un primer número'));
+  const numero2 = Number(await ask('Inserta un segundo número'));
+  const operacion = await ask('¿Cuál operación quieres realizar?');
 
-  const suma = num1 + num2;
-  // TODO: Implementar la resta, multiplicación y división
+  // const suma = numero1 + numero2;
+  // const resta = numero1 - numero2;
+  // const multiplicacion = numero1 * numero2;
+  // const division = numero1 / numero2;
 
-  console.log('Suma:', suma);
-  // TODO: Mostrar el resultado de la resta, multiplicación y división
+  // console.log(`El resultado de ${numero1} + ${numero2} es: ${suma}`);
+  // console.log(`El resultado de ${numero1} - ${numero2} es: ${resta}`);
+  // console.log(`El resultado de ${numero1} * ${numero2} es: ${multiplicacion}`);
+  // console.log(`El resultado de ${numero1} / ${numero2} es: ${division}`);
 
+  let resultado;
+
+  switch (operacion.toLowerCase()){
+    case "suma":
+      resultado = numero1 + numero2;
+      console.log(`El resultado de ${numero1} + ${numero2} es: ${resultado}`);
+      break;
+    case "resta":
+      resultado = numero1 - numero2;
+      console.log(`El resultado de ${numero1} - ${numero2} es: ${resultado}`);
+      break;
+    case "multiplicacion":
+      resultado = numero1 * numero2;
+      console.log(`El resultado de ${numero1} * ${numero2} es: ${resultado}`);
+      break;
+    case "division":
+      if(numero1 !== 0 && numero2 !== 0){
+        resultado = numero1 / numero2;
+        console.log(`El resultado de ${numero1} / ${numero2} es: ${resultado}`);
+      } else{
+        resultado = "Operación no valida, no se puede dividir entre 0";
+        console.log(resultado);
+      }
+      break;
+  }
 }
 
 main();
