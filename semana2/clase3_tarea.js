1132d0517775271c7fb6baca5688e46a68c051c3
Andhony <andhonyrpt@gmail.com>
2025-05-15 00:05:16 -0600

    Resolucion tarea clase 3

--- semana2/clase3_tarea.js
+++ semana2/clase3_tarea.js
@@ -9,3 +9,37 @@
 // Cuenta cuántos tienen 18 años o más.
 //
 // Muestra el total.
+
+const { ask } = require('../helpers/input');
+
+async function main() {
+
+    let opcion = '';
+    const edades =[];
+    let resultado = 0;
+
+    while(opcion !== '3'){
+        opcion = await ask(`¿Qué deseas hacer? \n1. Insertar un número \n2. Mostrar total \n3. Salir`);
+
+        if( opcion === '1'){
+
+            let edad = Number(await ask('Ingresa una edad: '));
+            edades.push(edad);
+
+        } else if ( opcion === '2'){
+
+            for(let index = 0; index < edades.length; index++ ){
+                if(edades[index] >= 18){
+                    resultado++
+                }
+            }
+            console.log("El total es:",resultado);
+            resultado = 0;
+        } else if( opcion === '3'){
+            //Salir
+            console.log('Gracias por usar el programa');
+        }
+    }
+}
+
+main();
