c2a430c648fbc5ed4d5aa28c56747e7dceacd140
Andhony <andhonyrpt@gmail.com>
2025-05-15 23:06:11 -0600

    Semana 2, día 4

--- semana2/ejercicio4.js
+++ semana2/ejercicio4.js
@@ -10,6 +10,48 @@ function obtenerPromedio(numeros) {
   return promedio;
 }
 
+function obtenerMayor(numeros){
+  let mayor = numeros[0];
+  for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] > mayor){
+      mayor = numeros[i];
+    }
+  }
+  return mayor;
+}
+
+function obtenerMenor(numeros){
+  let menor = numeros[0];
+  for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] < menor){
+      menor = numeros[i];
+    }
+  }
+  return menor;
+}
+
+function resumenEstadistico(numeros){
+
+  const promedio = obtenerPromedio(numeros);
+  const mayor = obtenerMayor(numeros);
+  const menor = obtenerMenor(numeros);
+
+  return { menor, mayor, promedio };
+}
+
+function iniciaVocal(nombres){
+  const vocales = ["a","e","i","o","u"];
+  const resultado = []
+
+  for(let i=0; i<nombres.length; i++){
+    const primeraLetra = nombres[i][0].toLowerCase();
+    if(vocales.includes(primeraLetra)){
+      resultado.push(nombres[i]);
+    }
+  }
+  return resultado;
+}
+
 async function main() {
   const edades = [20, 18, 25, 30, 22];
   const lista = [5, 20, 8, 99, 3];
@@ -17,6 +59,17 @@ async function main() {
   const nombres = ["Ana", "Esteban", "Luis", "Oscar", "María"];
 
   console.log(`Promedio = ${obtenerPromedio(edades)}`);
+  console.log(`Promedio = ${obtenerPromedio(lista)}`);
+  console.log(`Promedio = ${obtenerPromedio(datos)}`);
+  console.log(`El número mayor de ${lista} = ${obtenerMayor(lista)}`);
+  const estadistica = resumenEstadistico(datos);
+  console.log(`El resumen estadístico de: ${datos}`);
+  console.log(`El numero menor es: ${estadistica.menor}`);
+  console.log(`El número mayor es: ${estadistica.mayor}`);
+  console.log(`El promedio es: ${estadistica.promedio}`);
+
+  console.log("Lista de nombres: ", nombres);
+  console.log("Nombres solo con vocal: ", iniciaVocal(nombres));
 }
 
 main();
